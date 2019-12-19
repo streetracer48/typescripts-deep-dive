f1d397a65ecf20b096780676414d54088fb72856
Karim <38099690+streetracer48@users.noreply.github.com>
2019-12-19 10:48:13 +0200

    namespace and classes added

--- enosis.ts
+++ enosis.ts
@@ -1,1 +1,35 @@
 
+declare namespace GreetingLib {
+    interface LogOptions {
+        verbose?: boolean;
+    }
+    interface AlertOptions {
+        modal: boolean;
+        title?: string;
+        color?: string;
+    }
+}
+  
+#namespaces in one declaration:
+
+declare namespace GreetingLib.Options {
+    // Refer to via GreetingLib.Options.Log
+    interface Log {
+        verbose?: boolean;
+    }
+    interface Alert {
+        modal: boolean;
+        title?: string;
+        color?: string;
+    }
+}
+  
+  const myGreeter = new Greeter("hello, world");
+myGreeter.greeting = "howdy";
+myGreeter.showGreeting();
+
+class SpecialGreeter extends Greeter {
+    constructor() {
+        super("Very special greetings");
+    }
+}
